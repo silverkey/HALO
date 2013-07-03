95b8fd3378515951f3b773cc904ae0eddce6f2fb
remo <remo@Z800.szn>
2013-07-03 17:04:26 +0200

    added get_go.R

--- CNE_PIPELINE/CI_CS_ENS/get_homologs.R
+++ CNE_PIPELINE/CI_CS_ENS/get_homologs.R
@@ -10,11 +10,15 @@ sp2 = args[3]
 
 db = paste(sp1,'gene_ensembl',sep='_')
 hom = paste(sp2,'homolog_ensembl_gene',sep='_')
+inter = paste(sp2,'inter_paralog_ensembl_gene',sep='_')
 outfile = paste(sp1,sp2,'homologs.txt',sep='_')
 
 ensembl = useMart(mart,host="ensembl.org",dataset=db)
 
-# trubripes_inter_paralog_ensembl_gene return an error
-tab = getBM(attributes=c('ensembl_gene_id',hom),mart=ensembl)
+tab1 = getBM(attributes=c('ensembl_gene_id',hom),mart=ensembl)
+tab2 = getBM(attributes=c('ensembl_gene_id',inter),mart=ensembl,bmHeader=FALSE)
+colnames(tab1) = c('sp1','sp2')
+colnames(tab2) = c('sp1','sp2')
+tab = rbind(tab1,tab2)
 
-write.table(tab,file=outfile,sep='\t',row.names=F,quote=F)
+write.table(unique(tab),file=outfile,sep='\t',row.names=F,quote=F)
